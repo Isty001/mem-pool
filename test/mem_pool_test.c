e865639aed66e8b6012d9886d8cb2b9c0108321f
Farkasvölgyi István <isty001@gmail.com>
2016-11-08 08:16:29 +0100

    alignment

--- test/mem_pool_test.c
+++ test/mem_pool_test.c
@@ -7,8 +7,8 @@
 
 MU_TEST(test_pool)
 {
-    size_t expected = 10;
-    size_t align = alignof(max_align_t);
+    int expected = 10;
+    int align = alignof(max_align_t);
 
     expected = expected + (align - expected % align);
 
@@ -21,7 +21,7 @@ MU_TEST(test_pool)
     ptr3 = pool_alloc(pool);
 
     mu_assert_int_eq(expected, ptr2 - ptr1);
-    mu_assert(ptr3 - ptr2 > 10, "This should be in a new Buffer");
+    mu_assert(ptr3 - ptr2 > expected, "This should be in a new Buffer");
 
     mu_assert(pool_has_ptr(pool, ptr1), "This should be known by the pool");
     mu_assert(pool_has_ptr(pool, ptr3), "This should be known by the pool");
